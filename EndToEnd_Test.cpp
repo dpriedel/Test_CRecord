a8b7616bef6a17b827a0e53cb40031accf0935c1
David P. Riedel <davidp.riedel@gmail.com>
2023-02-11 10:20:43 -0500

    add some real code as a test

--- EndToEnd_Test.cpp
+++ EndToEnd_Test.cpp
@@ -23,9 +23,14 @@
 //  Description:
 // =====================================================================================
 
-
+#include <array>
+#include <charconv>
+#include <cmath>
+#include <cstdint>
 #include <filesystem>
 #include <fstream>
+#include <vector>
+
 // #include <future>
 //
 // #include <range/v3/algorithm/for_each.hpp>
@@ -34,12 +39,15 @@
 // #include <range/v3/algorithm/equal.hpp>
 // #include <range/v3/algorithm/find_if.hpp>
 
+#include <date/date.h>
+
 #include <fmt/format.h>
 #include <gtest/gtest.h>
 #include <spdlog/spdlog.h>
 
 #include <gmock/gmock.h>
 
+#include "utilities.h"
 #include "CRecord.h"
 #include "CRecordDescParser.h"
 
@@ -52,11 +60,25 @@ using namespace testing;
 
 std::shared_ptr<spdlog::logger> DEFAULT_LOGGER;
 
+// ===  FUNCTION  ======================================================================
+//         Name:  StringToDateYMD
+//  Description:  
+// =====================================================================================
+
+date::year_month_day StringToDateYMD(std::string_view input_format, std::string_view the_date)
+{
+    std::istringstream in{the_date.data()};
+    date::year_month_day result{};
+    date::from_stream(in, input_format.data(), result);
+    BOOST_ASSERT_MSG(! in.fail() && ! in.bad(), fmt::format("Unable to parse given date: {}", the_date).c_str());
+    return result;
+}		// -----  end of method StringToDateYMD  ----- 
+
 class TestInstantiateCRecord : public Test
 {
 };
 
-TEST_F(TestInstantiateCRecord, UseLibraryVersionForFixedRecord)    //NOLINT
+TEST_F(TestInstantiateCRecord, VerifyCompileLinkRunUsinglibCRecord)    //NOLINT
 {
 	try
     {
@@ -81,8 +103,98 @@ TEST_F(TestInstantiateCRecord, UseLibraryVersionForFixedRecord)    //NOLINT
 	{		// handle exception: unspecified
         spdlog::error("Something totally unexpected happened.");
 	}
-   EXPECT_TRUE(fs::exists("/tmp/test_charts/SPY_10X3_linear_eod.json"));
-   ASSERT_TRUE(fs::exists("/tmp/test_charts/SPY_10X1_linear_eod.json"));
+}
+
+TEST_F(TestInstantiateCRecord, UseCRecordToActuallyDoSomething)    //NOLINT
+{
+    struct StockData
+    {
+        std::array<char, 10> symbol = {};
+        date::year_month_day date = {};
+        float_t open = 0.0;
+        float_t high = 0.0;
+        float_t low = 0.0;
+        float_t close = 0.0;
+        int32_t volume = 0;
+
+        const size_t symbol_len = sizeof(symbol);
+    };
+
+    // fmt::print("empty StockData: {}\n", StockData{});
+
+	try
+    {
+        CRecordDescParser my_parser;
+    
+        auto new_record = my_parser.ParseRecordDescFile("./test_files/file4_Record_Desc");
+        ASSERT_TRUE(new_record);
+
+        /* verify got all fields */
+
+        auto& stock_data_record = std::get<e_VariableRecord>(new_record.value());
+
+        EXPECT_EQ(stock_data_record.GetFields().size(), 9);
+
+        std::string buffer{};
+        buffer.reserve(500);
+
+        std::ifstream file_data = std::ifstream("./test_files/file4_data.dat", std::ios::in | std::ios::binary);
+
+        // let's read our header record and pick up field names. 
+
+        std::getline(file_data, buffer);
+        stock_data_record.UseData(std::string_view{buffer.data(), buffer.size()});
+
+        std::vector<StockData> stock_data_history;
+        
+        auto to_number = [](std::string_view in_value, auto& out_value) {
+            auto [ptr, ec] { std::from_chars(in_value.begin(), in_value.end(), out_value) };
+            if (ec == std::errc())
+            {
+                // nothing to do
+            }
+            else if (ec == std::errc::invalid_argument)
+            {
+                std::cout << "That isn't a number.\n";
+            }
+            else if (ec == std::errc::result_out_of_range)
+            {
+                std::cout << "This number is larger than an int.\n";
+            }
+        };
+        while (std::getline(file_data, buffer))
+        {
+            stock_data_record.UseData(std::string_view{buffer.data(), buffer.size()});
+            StockData new_data;
+            new_data.symbol.fill('\0');
+            stock_data_record["Code"].copy(new_data.symbol.data(), new_data.symbol.size() - 1);
+            new_data.date = StringToDateYMD("%Y-%m-%d", stock_data_record["Date"]);
+            to_number(stock_data_record["Open"], new_data.open);
+            to_number(stock_data_record["High"], new_data.high);
+            to_number(stock_data_record["Low"], new_data.low);
+            to_number(stock_data_record["Close"], new_data.close);
+            to_number(stock_data_record["Volume"], new_data.volume);
+
+            stock_data_history.push_back(new_data);
+            // fmt::print("{}\n", stock_data_record);
+        }
+        file_data.close();
+
+        // I peeked so I know this number.
+
+        EXPECT_EQ(stock_data_history.size(), 5315);
+    }
+
+    // catch any problems trying to setup application
+
+	catch (const std::exception& theProblem)
+	{
+        spdlog::error(fmt::format("Something fundamental went wrong: {}", theProblem.what()));
+	}
+	catch (...)
+	{		// handle exception: unspecified
+        spdlog::error("Something totally unexpected happened.");
+	}
 }
 
 void InitLogging ()
